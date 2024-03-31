02b17ad56bb3507bddbb8e75b9a21aa7c9c29da2
benboatright <62475743+benboatright@users.noreply.github.com>
2024-03-31 18:14:38 -0400

    update numbers exercises

--- numbers/number.rs
+++ numbers/number.rs
@@ -1,11 +1,11 @@
 // 1.
-fn main() {
-    let x: i32 = 5;
-    let mut _y = 5;
-    _y = x;
-    let _z = 10;
-    println!("Success!{}",x)
-}
+// fn main() {
+//     let x: i32 = 5;
+//     let mut _y = 5;
+//     _y = x;
+//     let _z = 10;
+//     println!("Success!{}",x)
+// }
 
 // 2.
 // fn main(){
@@ -92,3 +92,37 @@ fn main() {
 //     println!("Success!");
 
 // }
+
+//11.
+fn main() {
+    //Integer addition
+    assert!(1u32 + 2 == 3u32);
+
+    //Integer subtraction
+    assert!(1i32 - 2 == -1i32);
+    assert!(1i8 - 2 == -1i8);
+
+    //Multiplication
+    assert!(3*50 == 150i32);
+
+    //Division
+    assert!(9.6f32 / 3.2f32 == 3.0f32);
+
+    //Modulus
+    assert!(24 % 5 == 4);
+
+    //Boolean Logic
+    assert!(true && false == false);
+    assert!(true || false == true);
+    assert!(!true == false);
+
+    //Bitwise operations
+    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
+    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+    println!("1 << 5 is {}", 1u32 << 5);
+    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
+
+    println!("Success!");
+
+}
